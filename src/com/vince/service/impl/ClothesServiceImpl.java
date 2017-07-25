6489c84dedfa6a419d5e286ca26245c75edd621a
vince <1791705739@qq.com>
2017-07-25 11:03:32 +0800

    update product buy

--- src/com/vince/service/impl/ClothesServiceImpl.java
+++ src/com/vince/service/impl/ClothesServiceImpl.java
@@ -3,6 +3,7 @@ package com.vince.service.impl;
 import com.vince.bean.Clothes;
 import com.vince.service.ClothesService;
 import com.vince.utils.BusinessException;
+import com.vince.utils.ClothesIO;
 import com.vince.utils.ProductsXmlUtils;
 
 import java.util.List;
@@ -11,9 +12,19 @@ import java.util.List;
  * Created by vince on 2017/7/20.
  */
 public class ClothesServiceImpl implements ClothesService {
+
+    private ClothesIO clothesIO = new ClothesIO();
     @Override
     public List<Clothes> list() throws BusinessException {
-        List<Clothes> clothes = ProductsXmlUtils.parserProductFormXml();
-        return clothes;
+        return clothesIO.list();
+    }
+
+    @Override
+    public Clothes findById(String cid) throws BusinessException {
+        return clothesIO.findById(cid);
+    }
+
+    public void update()throws BusinessException{
+        clothesIO.update();
     }
 }
--- src/com/vince/ui/HomeClass.java
+++ src/com/vince/ui/HomeClass.java
@@ -1,16 +1,26 @@
 package com.vince.ui;
 
 import com.vince.bean.Clothes;
+import com.vince.bean.Order;
+import com.vince.bean.OrderItem;
 import com.vince.service.ClothesService;
+import com.vince.service.OrderService;
 import com.vince.service.impl.ClothesServiceImpl;
+import com.vince.service.impl.OrderServiceImpl;
+import com.vince.utils.BusinessException;
 import com.vince.utils.ConsoleTable;
+import com.vince.utils.DateUtils;
 
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by vince on 2017/7/20.
  */
 public class HomeClass extends BaseClass {
+
+    private OrderService orderService = new OrderServiceImpl();
+    private ClothesService clothesService = new ClothesServiceImpl();
 
     public void show(){
         showProducts();
@@ -30,8 +40,12 @@ public class HomeClass extends BaseClass {
                     flag = false;
                     break;
                 case "3": //3、购买
-                    buyProducts();
-                    flag = false;
+                    try {
+                        buyProducts();
+                        flag = false;
+                    }catch ( BusinessException e){
+                        println(e.getMessage());
+                    }
                     break;
                 case "0": //0、退出
                     flag = false;
@@ -44,8 +58,58 @@ public class HomeClass extends BaseClass {
         }
     }
 
-    private void buyProducts() {
+    /**
+     * 购买商品
+     * @throws BusinessException
+     */
+    private void buyProducts()throws BusinessException {
+        //生成订单
+        boolean flag = true;
+        int count = 1;
+        float sum = 0.0f; //订单总金额
+        Order order = new Order(); //生成的订单
+        while (flag){
+            println(getString("product.input.id"));
+            String id = input.nextLine();
+            println(getString("product.input.shoppingNum"));
+            String shoppingNum = input.nextLine();
+            OrderItem orderItem = new OrderItem();
+            Clothes clothes = clothesService.findById(id);
 
+            int num = Integer.parseInt(shoppingNum);
+            if(num>clothes.getNum()){
+                throw new BusinessException("product.num.error");
+            }
+            //一条订单明细
+            clothes.setNum(clothes.getNum()-num);//减去库存
+            orderItem.setClothes(clothes);
+            orderItem.setShoppingNum(num);
+            orderItem.setSum(clothes.getPrice()*num);
+            sum += orderItem.getSum();
+            orderItem.setItemId(count++);
+            order.getOrderItemList().add(orderItem);
+
+            println(getString("product.buy.continue"));
+            String isBuy = input.nextLine();
+            switch (isBuy){
+                case "1":
+                    flag = true;
+                    break;
+                case "2":
+                    flag = false;
+                    break;
+                default:
+                    flag = false;
+                    break;
+            }
+        }
+        order.setCreateDate(DateUtils.toDate(new Date()));
+        order.setUserId(currUser.getId());
+        order.setSum(sum);
+        order.setOrderId(orderService.list().size()+1);
+        orderService.buyProduct(order);
+        clothesService.update();
+        showProducts();
     }
 
     private void findOrderById() {
@@ -55,7 +119,6 @@ public class HomeClass extends BaseClass {
     }
 
     private void showProducts() {
-        ClothesService clothesService = new ClothesServiceImpl();
         List<Clothes> list = clothesService.list();
         ConsoleTable t = new ConsoleTable(8, true);
         t.appendRow();
